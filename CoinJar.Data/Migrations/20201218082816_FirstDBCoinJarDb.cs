eb13f4c099a358ade193cf5de932a36c568cf8ba
WouterLombard <wouter@skrmiish.com>
2020-12-18 12:46:38 +0200

    Add project files.

---
+++ CoinJar.Data/Migrations/20201218082816_FirstDBCoinJarDb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoinJar.Data.Migrations
+{
+    public partial class FirstDBCoinJarDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coin",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Amount = table.Column<decimal>(type: "decimal(18,4)", nullable: false),
+                    Volume = table.Column<decimal>(type: "decimal(18,4)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coin", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coin");
+        }
+    }
+}
